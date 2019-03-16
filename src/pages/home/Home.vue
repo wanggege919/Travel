de3cfc5677a49acbe38664c5b01ed2b039bb31bb
wanggege <1914161290@qq.com>
2019-03-17 00:56:32 +0800

    添加轮播

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <HomeHeader></HomeHeader>
+    <HomeSwiper></HomeSwiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
     name: 'Home',
     components:{
-        HomeHeader
+        HomeHeader,
+        HomeSwiper
     }
 };
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+      <!-- slides -->
+      <swiper-slide v-for='item in swiperList'>
+          <img  class="swiper-img" :src="item.imgUrl" key="item.id">
+      </swiper-slide>
+     
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeSwiper",
+  data(){
+      return {
+          swiperOption:{
+              pagination: '.swiper-pagination',
+              loop: true
+          },
+          swiperList:[
+              {
+                  id: '0001',
+                  imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/ed5f4115226306e48e6fad106a038afe.jpg_750x200_64df48d6.jpg'
+              },
+              {
+                  id: '0002',
+                  imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/980adfa646fede8d7a4c46feabaac6ee.jpg_750x200_63ecd371.jpg'
+              },
+              {
+                  id: '0003',
+                  imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/2f5f3ea4698c9b7898db7562d89b91ed.jpg_750x200_bd3b4ce9.jpg'
+              }
+          ]
+      }
+  }
+};
+</script>
+<style lang='stylus' scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #fff
+    .wrapper
+        overflow hidden
+        width 100%
+        height  0
+        padding-bottom 26.67%
+        background #eee
+        .swiper-img
+            width 100%
+</style>
